0c9d48f2e39b8e7c2967faf06e7e42a884eaf78c
Nicholas <nicholas.nmalinda@gmail.com>
2022-06-16 10:29:42 +0300

    Lab Complete

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+   return fetch('https://anapioficeandfire.com/api/books')
+   .then(response=>response.json())
+ 
+   .then(jsonData=> {renderBooks(jsonData)  })}
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -13,5 +16,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks() 
+  
 });
